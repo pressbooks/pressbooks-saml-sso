c3732d4e530510826d8ea01b488ad412a4a7263d
Ricardo Aragon <richard015ar@gmail.com>
2021-06-21 14:20:52 -0400

    Reverting tests/bootstrap.php changes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,9 +8,6 @@ if ( ! $_tests_dir ) {
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
-// Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
-
 /**
  * Manually load the plugin being tested.
  */
@@ -20,5 +17,8 @@ function _manually_load_plugin() {
 	require_once( __DIR__ . '/../../pressbooks/requires-admin.php' );
 	require_once( __DIR__ . '/../pressbooks-saml-sso.php' );
 }
-_manually_load_plugin();
+
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';

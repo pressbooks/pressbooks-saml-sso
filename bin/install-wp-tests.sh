fbc294228763df739ed6bdbb19d3d90d3e960683
Ricardo Aragon <richard015ar@gmail.com>
2021-10-06 17:16:11 -0400

    Fix pipeline for wp 5.8

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -44,27 +44,27 @@ set -ex
 install_wp() {
 
 	if [ -d $WP_CORE_DIR ]; then
-  		return;
-  	fi
+		return;
+	fi
 
-  	mkdir -p $WP_CORE_DIR
+	mkdir -p $WP_CORE_DIR
 
-  	if [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
-  		mkdir -p /tmp/wordpress-nightly
-  		download https://wordpress.org/nightly-builds/wordpress-latest.zip  /tmp/wordpress-nightly/wordpress-nightly.zip
-  		unzip -q /tmp/wordpress-nightly/wordpress-nightly.zip -d /tmp/wordpress-nightly/
-  		mv /tmp/wordpress-nightly/wordpress/* $WP_CORE_DIR
-  	else
-  		if [ $WP_VERSION == 'latest' ]; then
-  			local ARCHIVE_NAME='latest'
-  		else
-  			local ARCHIVE_NAME="wordpress-$WP_VERSION"
-  		fi
-  		download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  /tmp/wordpress.tar.gz
-  		tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
-  	fi
+	if [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
+		mkdir -p /tmp/wordpress-nightly
+		download https://wordpress.org/nightly-builds/wordpress-latest.zip  /tmp/wordpress-nightly/wordpress-nightly.zip
+		unzip -q /tmp/wordpress-nightly/wordpress-nightly.zip -d /tmp/wordpress-nightly/
+		mv /tmp/wordpress-nightly/wordpress/* $WP_CORE_DIR
+	else
+		if [ $WP_VERSION == 'latest' ]; then
+			local ARCHIVE_NAME='latest'
+		else
+			local ARCHIVE_NAME="wordpress-$WP_VERSION"
+		fi
+		download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  /tmp/wordpress.tar.gz
+		tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
+	fi
 
-  	download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIR/wp-content/db.php
+	download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIR/wp-content/db.php
 }
 
 install_test_suite() {
